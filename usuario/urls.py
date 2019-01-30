8ac8daa2b4a7805ec28c5aecdab1fbfc30732320
vikvik98 <vinicius.c.mascarenhas@hotmail.com>
2019-01-30 18:03:19 -0300

    Template login e register e bootstrap

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from usuario.views import *
+from usuario.views import RegistrarUsuarioView
 
 urlpatterns = [
-
     path('registrar/', RegistrarUsuarioView.as_view(), name='registrar'),
 ]
