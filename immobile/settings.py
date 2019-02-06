3d40f30fdd3cc23b120e7faded47a8de0c4c691d
micaelleal <mycaellmoura@hotmail.com>
2019-02-06 12:00:54 -0300

    Validação registrar

--- immobile/settings.py
+++ immobile/settings.py
@@ -115,7 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/imovel/home/'
+LOGOUT_REDIRECT_URL = '/usuario/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- usuario/forms.py
+++ usuario/forms.py
@@ -1,14 +1,42 @@
 from django import forms
-from django.forms.utils import ErrorList
+from django.contrib.auth.models import User
 
 
 class PerfilForm(forms.Form):
-
-    nome = forms.CharField(max_length=50)
+    nome = forms.CharField(max_length=50, min_length=4)
     telefone = forms.CharField(max_length=20)
     email = forms.EmailField()
-    senha = forms.CharField(max_length=65, widget=forms.PasswordInput)
+    senha = forms.CharField(max_length=65, min_length=3,
+                            widget=forms.PasswordInput)
 
     def clean_nome(self):
         nome = self.cleaned_data['nome']
-        return nome.lower()
+        return nome.capitalize()
+
+    def clean_telefone(self):
+        telefone = self.cleaned_data['telefone']
+        telefone = telefone.replace('-', '').replace('(', "").replace(")", "").replace(" ", "")
+
+        try:
+            int(telefone)
+            if len(telefone) > 11 or len(telefone) < 10:
+                raise ValueError
+        except ValueError:
+            raise forms.ValidationError('Valor passado é inválido')
+
+        telefone_format = "(" + telefone[0:2] + ") "
+        if len(telefone) == 10:
+            telefone_format += telefone[2:6] + "-" + telefone[6:10]
+        else:
+            telefone_format += telefone[2:7] + "-" + telefone[7:11]
+
+        print(telefone)
+        return telefone_format
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        users = User.objects.filter(email=email)
+
+        if users:
+            raise forms.ValidationError('O e-mail passado já está sendo usado')
+        return email
